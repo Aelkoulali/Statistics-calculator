46fecb7f2b08ffeb8f5d34eb3f85127036146f35
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-16 10:06:47 +0200

    Add callback function to .map( )

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Add calculate function
 const calculate = () => {
     const value = document.querySelector("#numbers").value; // value input
-    const array = value.split(/,\s*/g); // 
+    const array = value.split(/,\s*/g);  
+    const numbers = array.map(el =>{
+        Number(value); 
+    });
 }
