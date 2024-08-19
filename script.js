a60bf73b3dcb2413c9697186d05a0f6f606fac17
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-08-19 11:11:10 +0200

    How t calculate the occurrence

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ console.log(oddListMedian);
 const evenListMedian = getMean([testArr2[testArr2.length / 2 - 1], testArr2[testArr2.length / 2]]);
 console.log(evenListMedian);
 */
+
+/*
+To calculate the occurrence you can use the following approach:
+const numbersArr = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4];
+const counts = {};
+numbersArr.forEach((el) => {
+  if (counts[el]) {
+    counts[el] += 1;
+  } else {
+    counts[el] = 1;
+  }
+});
+
+Check if the current number is already in the counts object. If it is, increment it by 1. If it is not, set it to 1.
+Resulting object. The keys are the numbers from the array and the values are the number of times each number appears in the list:
+{ 1: 3, 2: 3, 3: 3, 4: 3, 5: 2 }
+*/
